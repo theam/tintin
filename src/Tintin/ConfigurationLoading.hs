ad19741d649bac369b98b917d1b3ed0da5604505
Javier Toledo <javier@theam.io>
2019-05-23 19:18:06 +0100

    Improvements in the config loader
    * Having a package.yaml or a cabal file in the current directory is now optional
    * Project config can be overriden in the `.tintin.yml` file

--- src/Tintin/ConfigurationLoading.hs
+++ src/Tintin/ConfigurationLoading.hs
@@ -17,21 +17,123 @@ import qualified Universum.Unsafe as Unsafe
 import Text.Read (read)
 import qualified Text.Inflections as Inflections
 
-
 data ConfigurationLoading
 
-readFileIfExists :: ( Has Logging.Capability eff
+data ProjectContext = ProjectContext
+  { name            :: Maybe Text
+  , synopsis        :: Maybe Text
+  , github          :: Maybe Text
+  , location        :: Maybe Text
+  , author          :: Maybe Text
+  , projectLocation :: Filesystem.Path
+  }
+
+getFieldValue :: Text -> Text -> Maybe Text
+getFieldValue field txt = txt
+                        & lines
+                        & filter (\t -> field `Text.isPrefixOf` Text.strip t)
+                        & safeHead
+                        & fmap Text.strip
+                        & flatMap (Text.stripPrefix $ field <> ":")
+                        & fmap Text.strip
+
+loadProjectConfig :: ( Has Logging.Capability eff
+                     , Has Filesystem.Capability eff
+                     )
+                   => Filesystem.Path -> Effectful eff ProjectContext
+loadProjectConfig currentPath = do
+  files <- Filesystem.list currentPath
+  let packageYamlFile = find isPackageYaml files
+  let cabalFile       = find isCabalFile files
+  case packageYamlFile <|> cabalFile of
+    Just file -> do
+      Logging.debug "Reading project info"
+      projectConfigFile <- Filesystem.readFile file
+      return ProjectContext
+        { name            = projectConfigFile & getFieldValue "name"
+        , synopsis        = projectConfigFile & getFieldValue "synopsis"
+        , github          = projectConfigFile & getFieldValue "github"
+        , location        = projectConfigFile & getFieldValue "location"
+        , author          = projectConfigFile & getFieldValue "author"
+        , projectLocation = currentPath
+        }
+    Nothing -> return ProjectContext
+      { name            = Nothing
+      , synopsis        = Nothing
+      , github          = Nothing
+      , location        = Nothing
+      , author          = Nothing
+      , projectLocation = currentPath
+      }
+ where
+  isPackageYaml (Filesystem.Path p) = p == "package.yaml"
+  isCabalFile   (Filesystem.Path p) = ".cabal" `Text.isInfixOf` p
+
+loadTintinConfig :: ( Has Logging.Capability eff
                     , Has Filesystem.Capability eff
                     )
-                 => Text -> Text -> Effectful eff (Maybe Text)
-readFileIfExists currentDir filename = do
-  let path = Filesystem.Path $ currentDir <> "/" <> filename
-  Logging.debug $ "Reading " <> filename
-  fileExists <- Filesystem.doesExist path
-  if fileExists then do
-    text <- Filesystem.readFile path
-    return (Just text)
-  else return Nothing
+                 => [Project.Page]
+                 -> ProjectContext
+                 -> Effectful eff Project.Info
+loadTintinConfig pages ctx = do
+  let Filesystem.Path currentPath = projectLocation ctx
+      tintinPath = Filesystem.Path $ currentPath <> "/.tintin.yml"
+  tintinExists <- Filesystem.doesExist tintinPath
+  unless tintinExists $ Filesystem.writeFile tintinPath "color: blue\n"
+  tintinConfig <- Filesystem.readFile tintinPath
+  let
+    projectName     = (tintinConfig & getFieldValue "name") <|> (name ctx)
+    projectSynopsis = (tintinConfig & getFieldValue "synopsis") <|> (synopsis ctx)
+    projectGithub   = (tintinConfig & getFieldValue "github") <|> (github ctx) <|> (location ctx)
+    projectAuthor   = (tintinConfig & getFieldValue "author") <|> (author ctx)
+    tintinColor     = tintinConfig & getFieldValue "color"
+    tintinLogo      = tintinConfig & getFieldValue "logo"
+  when (isNothing projectName) (Errors.showAndDie ["Project must have a name. Please set it in package.yaml or *.cabal."])
+  when (isNothing projectSynopsis) (Errors.showAndDie ["Project must have a synopsis. Please set it in package.yaml or *.cabal."])
+  when (isNothing tintinColor) (Errors.showAndDie [errorMessages])
+  return Project.Info
+    { name = Unsafe.fromJust projectName
+    , synopsis = Unsafe.fromJust projectSynopsis
+    , githubLink = parseGithubUrl <$> projectGithub
+    , githubAuthor = projectAuthor
+    , color = makeColor $ Unsafe.fromJust tintinColor
+    , logoUrl = tintinLogo
+    , pages = pages
+    }
+ where
+  parseGithubUrl txt =
+    let unquoted = stripGit $ stripQuotes txt
+    in unquoted
+       & Text.stripPrefix "github.com/"
+       & fromMaybe unquoted
+
+  stripGit txt =
+    txt
+      & Text.stripPrefix "git://"
+      & flatMap (Text.stripSuffix ".git")
+      & fromMaybe txt
+
+  stripQuotes txt =
+    txt
+    & Text.stripPrefix "\""
+    & flatMap (Text.stripSuffix "\"")
+    & fromMaybe txt
+
+  errorMessages = unlines
+    ["Tintin usually generates a .tintin.yml file with a color configuration. Maybe you don't have enough permissions?"
+    , ""
+    , ""
+    , "Try creating .tintin.yml and adding color:blue to it."
+    ]
+
+makeColor :: Text -> Project.Color
+makeColor txt =
+  case Text.stripPrefix "#" txt of
+    Just _ -> Project.HexColor txt
+    Nothing -> Inflections.SomeWord <$> Inflections.mkWord txt
+      & Inflections.titleize
+      & toString
+      & read
 
 loadInfo :: ( Has Logging.Capability eff
             , Has Filesystem.Capability eff
@@ -41,103 +143,6 @@ loadInfo :: ( Has Logging.Capability eff
 loadInfo htmlFiles = do
   let pages = htmlFiles
               & map (\HtmlFile.HtmlFile {..} -> Project.Page title content filename)
-  Filesystem.Path currentDir <- Filesystem.currentDirectory
-  files <- Filesystem.list (Filesystem.Path currentDir)
-  let packageYamlFile = find isPackageYaml files
-  let cabalFile       = find isCabalFile files
-  case packageYamlFile <|> cabalFile of
-    Nothing -> do
-      Errors.showAndDie ["No package.yaml or *.cabal file found."]
-      error ""
-
-    Just p -> do
-      let tintinPath = Filesystem.Path $ currentDir <> "/.tintin.yml"
-      Logging.debug "Reading project info"
-      projectInfoFile <- Filesystem.readFile p
-      tintinExists    <- Filesystem.doesExist tintinPath
-      unless tintinExists $
-        Filesystem.writeFile tintinPath "color: blue\n"
-      tintinFile <- Filesystem.readFile tintinPath
-      let
-        projectName     = projectInfoFile & getFieldValue "name"
-        projectSynopsis = projectInfoFile & getFieldValue "synopsis"
-        projectGithub   = (projectInfoFile & getFieldValue "github")
-                          <|> (projectInfoFile & getFieldValue "location")
-        projectAuthor   = projectGithub & fmap getAuthor
-        tintinColor     = tintinFile & getFieldValue "color"
-        tintinLogo      = tintinFile & getFieldValue "logo"
-      when (isNothing projectName) (Errors.showAndDie ["Project must have a name. Please set it in package.yaml or *.cabal."])
-      when (isNothing projectSynopsis) (Errors.showAndDie ["Project must have a synopsis. Please set it in package.yaml or *.cabal."])
-      when (isNothing tintinColor)
-        (Errors.showAndDie [errorMessages])
-      return Project.Info
-        { name = Unsafe.fromJust projectName
-        , synopsis = Unsafe.fromJust projectSynopsis
-        , githubLink = parseGithubUrl <$> projectGithub
-        , githubAuthor = projectAuthor
-        , color = makeColor $ Unsafe.fromJust tintinColor
-        , logoUrl = tintinLogo
-        , pages = pages
-        }
-
- where
-  errorMessages = unlines
-    ["Tintin usually generates a .tintin.yml file with a color configuration. Maybe you don't have enough permissions?"
-    , ""
-    , ""
-    , "Try creating .tintin.yml and adding color:blue to it."
-    ]
-  isPackageYaml (Filesystem.Path p) =
-    p == "package.yaml"
-
-  isCabalFile   (Filesystem.Path p) =
-    ".cabal" `Text.isInfixOf` p
-
-  makeColor :: Text -> Project.Color
-  makeColor txt =
-    case Text.stripPrefix "#" txt of
-      Just _ -> Project.HexColor txt
-      Nothing -> Inflections.SomeWord <$> Inflections.mkWord txt
-        & Inflections.titleize
-        & toString
-        & read
-
-  getFieldValue field txt = txt
-                          & lines
-                          & filter (\t -> field `Text.isPrefixOf` Text.strip t)
-                          & safeHead
-                          & fmap Text.strip
-                          & flatMap (Text.stripPrefix $ field <> ":")
-                          & fmap Text.strip
-  getAuthor txt =
-    let unquoted = stripQuotes txt
-    in parseGithubUrl unquoted
-       & (\t -> if "http" `Text.isPrefixOf` t
-                then Text.splitOn "/" t
-                     & filter (not . Text.isInfixOf "git")
-                     & filter (not . Text.null)
-                     & Unsafe.tail
-                     & Unsafe.head
-                else t
-         )
-       & Text.takeWhile (/= '/')
-
-  parseGithubUrl txt =
-    let unquoted = stripGit $ stripQuotes txt
-    in unquoted
-       & Text.stripPrefix "github.com/"
-       & fromMaybe unquoted
-
-  stripQuotes txt =
-    txt
-    & Text.stripPrefix "\""
-    & flatMap (Text.stripSuffix "\"")
-    & fromMaybe txt
-
-  stripGit txt =
-    txt
-    & Text.stripPrefix "git://"
-    & flatMap (Text.stripSuffix ".git")
-    & fromMaybe txt
-
-
+  Filesystem.currentDirectory
+    >>= loadProjectConfig
+    >>= loadTintinConfig pages
